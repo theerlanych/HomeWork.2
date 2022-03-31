ac67c9b4c9783b9504b86d4334ec2e0f03dc9719
theerlanych <theerlanych@gmail.com>
2022-03-31 20:05:21 +0600

    ДЗ 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,20 +16,19 @@ public class Main {
 
     public static String student(int age, int temperature) {
         if ((age > 20 && age < 45) && (temperature > -20 && temperature < 30)) {
-            System.out.println("Можно идти гулять");
+            return ("Можно идти гулять");
 
         } else if ((age < 20) && (temperature >= 0 && temperature < 28)) {
-            System.out.println("Можно идти гулять");
+            return ("Можно идти гулять");
         } else if ((age < 45) && (temperature > -10 && temperature < 25)) {
-            System.out.println("Можно идти гулять");
+            return ("Можно идти гулять");
 
         } else {
-            System.out.println("Оставайтесь дома");
+            return ("Оставайтесь дома");
         }
-        return "age = " + age + "temperature = " + temperature;
-
 
     }
+
 
     public static int generateRandomAge() {
         Random random = new Random();
@@ -37,4 +36,3 @@ public class Main {
         return age;
     }
 }
-
